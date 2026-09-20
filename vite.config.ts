e7d9ef209ac603bd06dfc5d8ff02e9d628bbd6a3
vishalsarkarofficial2-star <vishalsarkarofficial2@gmail.com>
2026-09-20 21:19:04 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: './suruchifoodtraining-1.0/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
